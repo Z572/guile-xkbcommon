e0f1fb2675d0b5d4b6aac12793c9314892edb66d
Zheng Junjie <zhengjunjie@iscas.ac.cn>
2024-11-09 20:07:50 +0800

    make this repo to a guix channel.

---
+++ .guix/modules/guile-xkbcommon-package.scm
@@ -0,0 +1,58 @@
+(define-module (guile-xkbcommon-package)
+  #:use-module (guile-bytestructure-class-package)
+  #:use-module (guix utils)
+  #:use-module (guix packages)
+  #:use-module ((guix licenses) #:prefix license:)
+  #:use-module (gnu packages xorg)
+  #:use-module (guix download)
+  #:use-module (guix git-download)
+  #:use-module (gnu packages gettext)
+  #:use-module (guix gexp)
+  #:use-module (gnu packages gl)
+  #:use-module (gnu packages xdisorg)
+  #:use-module (guix build-system gnu)
+  #:use-module (gnu packages bash)
+  #:use-module (gnu packages)
+  #:use-module (gnu packages autotools)
+  #:use-module (gnu packages guile)
+  #:use-module (gnu packages gtk)
+  #:use-module (gnu packages guile-xyz)
+  #:use-module (gnu packages ibus)
+  #:use-module (gnu packages pkg-config)
+  #:use-module (gnu packages texinfo)
+  #:use-module (gnu packages wm)
+  #:use-module (gnu packages xml)
+  #:use-module (gnu packages freedesktop))
+
+(define-public guile-xkbcommon
+  (package
+    (name "guile-xkbcommon")
+    (version "git")
+    (source (local-file "../.." "guile-xkbcommon-checkout"
+                        #:recursive? #t
+                        #:select? (or (git-predicate
+                                       (string-append
+                                        (current-source-directory)
+                                        "/../.."))
+                                      (const #t))))
+    (build-system gnu-build-system)
+    (arguments
+     (list #:make-flags #~(list "GUILE_AUTO_COMPILE=0")))
+    (native-inputs
+     (list autoconf automake
+           pkg-config
+           guile-3.0-latest))
+    (inputs (list guile-3.0-latest
+                  libxkbcommon
+                  ;;; xkbregistry pc file require
+                  libxml2))
+    (propagated-inputs
+     (list
+      guile-bytestructure-class
+      guile-bytestructures))
+    (synopsis "")
+    (description "")
+    (home-page "")
+    (license license:gpl3+)))
+
+guile-xkbcommon
---
+++ channels.scm
@@ -0,0 +1,17 @@
+(list (channel
+       (name 'guix)
+       (url "https://git.savannah.gnu.org/git/guix.git")
+       (branch "master")
+       (commit
+        "f5ca62fcadf7a1a61cb13dc89edcfd3e9884e9e0")
+       (introduction
+        (make-channel-introduction
+         "9edb3f66fd807b096b48283debdcddccfea34bad"
+         (openpgp-fingerprint
+          "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
+      (channel
+       (name 'guile-bytestructure-class)
+       (url "https://github.com/Z572/guile-bytestructure-class")
+       (branch "main")
+       (commit
+        "353c3bd8b0cebba3582e3d279326bbb95858b902")))

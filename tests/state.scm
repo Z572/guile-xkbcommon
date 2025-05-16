3c7ab570df1960acbfb10faad68977819d57c905
Zheng Junjie <z572@z572.online>
2025-05-16 12:10:09 +0800

    test xkb-state-new when keymap is not <xkb-state>.

--- tests/state.scm
+++ tests/state.scm
@@ -5,4 +5,8 @@
 (test-assert "xkb-state-new"
   (xkb-state-new (xkb-keymap-new (xkb-context-new))))
 
+(test-error "xkb-state-new: fail"
+            #t
+            (xkb-state-new #f))
+
 (test-end "state")
